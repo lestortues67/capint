f6ebaf4090066901c1decaa047ba352a361a392a
christian doriath <christian.doriath@free.fr>
2026-06-14 12:21:18 +0200

    ajout fichier main.c

---
+++ main/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "esp_log.h" // Pour les fonctions de log d'ESP-IDF
+#include "capteur_temp.h"
+
+static const char *TAG = "MY_APP";
+
+void app_main(void)
+{
+	ESP_LOGI(TAG, "Application principale (app_main) démarrée.");
+
+}
